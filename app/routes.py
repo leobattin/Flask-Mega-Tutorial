07b777aa838993027013c8dfd7bd870e1b6b0bd0
Léo Battin <battin@siel42.fr>
2020-07-17 11:06:30 +0200

    end of chapter 5 - User Logins

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,15 @@
 import random
-from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Léo'}
     if random.choice([True, False]):
         title = 'Home'
     else:
@@ -18,18 +21,45 @@ def index():
             'body': 'Beautiful day in Portland!'
         },
         {
-            'author': user,
+            'author': current_user,
             'body': 'The Avengers movie was so cool!'
         }
     ]
 
-    return render_template('index.jinja', title=title, user=user, posts=posts)
+    return render_template('index.jinja', title=title, user=current_user, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return(redirect(url_for('index'))) # User already autheticated
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page) # Authentication successful
+    return render_template('login.jinja', title='Sign In', form=form) # Form validation failed
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
-    return render_template('login.jinja', title='Sign In', form=form)
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user !')
+        return redirect(url_for('login'))
+    return render_template('register.jinja', title='Register', form=form)
